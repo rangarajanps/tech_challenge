af5e5c0efbf176924639451067b242f26a9a21eb
Rangarajan Padur Sivaraman <rangarajanps@gmail.com>
2021-01-17 23:46:07 +0000

    added 2nd input test

--- challenge3/q3solution_test.py
+++ challenge3/q3solution_test.py
@@ -3,9 +3,15 @@ from q3solution import get_from_nested
 dictdata = {"a":{"b":{"c":"d"}}}
 print("Object : ", dictdata)
 
-print("key:  a   value: ", get_from_nested(dictdata,"a","/"))
-print("key:  a/b   value: ", get_from_nested(dictdata,"a/b","/"))
-print("key:  a/b/c   value: ", get_from_nested(dictdata,"a/b/c","/"))
-print("key:  x   value: ", get_from_nested(dictdata,"x","/"))
-print("key:  a/c   value: ", get_from_nested(dictdata,"a/c","/"))
-print("key:  a/b/e  value: ", get_from_nested(dictdata,"a/b/e","/"))
+print("key: a, value: ", get_from_nested(dictdata,"a","/"))
+print("key: a/b, value: ", get_from_nested(dictdata,"a/b","/"))
+print("key: a/b/c, value: ", get_from_nested(dictdata,"a/b/c","/"))
+print("key: x, value: ", get_from_nested(dictdata,"x","/"))
+print("key: a/c, value: ", get_from_nested(dictdata,"a/c","/"))
+print("key: a/b/e, value: ", get_from_nested(dictdata,"a/b/e","/"))
+
+print("\n")
+dictdata = {"x":{"y":{"z":"a"}}}
+print("Object : ", dictdata)
+
+print("key:  x/y/z, value: ", get_from_nested(dictdata,"x/y/z","/"))
